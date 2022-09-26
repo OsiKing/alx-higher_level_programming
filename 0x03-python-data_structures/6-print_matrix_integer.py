8c117852a879e3e49790183aa27b7efbc36543ae
OsiKing <c.o.enekhaze@gmail.com>
2022-09-26 08:00:14 -0700

    6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        for m in range(len(matrix[i])):
+            print("{:d}".format(matrix[i][m]), end="")
+            if m != (len(matrix[i]) - 1):
+                print(" ", end="")
+
+        print("")
