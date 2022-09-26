f1f29ff66ac72225ece542a4b01dcf79946c2614
Callistus Enekhaze <44670996+OsiKing@users.noreply.github.com>
2022-09-26 16:47:30 +0100

    Update 10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-def divisible_by_2(my_list[]):
-    mults = []
+def divisible_by_2(my_list=[]):
+    multiples = []
     for i in range(len(my_list)):
-        if my_list[i] % 2 = 0:
-            mults.append(True)
+        if my_list[i] % 2 == 0:
+            multiples.append(True)
         else:
-            mults.append(False)
+            multiples.append(False)
 
-    return (mults)
+    return (multiples)
